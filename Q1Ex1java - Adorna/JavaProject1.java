ec3ad1f5e07480e368aa652ca5088fed889e5f6d
Kendronks <80277638+Kendronks@users.noreply.github.com>
2022-09-07 08:58:27 +0800

    Add files via upload

---
+++ Q1Ex1java - Adorna/JavaProject1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaproject1;
+
+
+/**
+ *
+ * @author TAU
+ */
+public class JavaProject1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String person1 = "JP";
+        String person2 = "Tabio";
+        String person3 = "Jian";
+        int Tage = 69;
+        int JPage = 21;
+        int JiAge = 420;
+        double tChemgrade = 70.42;
+        double jpChemgrade = 68.42;
+        double jianChemgrade = 82.19;
+        
+        System.out.printf("name: %s \n age: %d \n Chemistry Grade: %f \n \n",
+                person1, JPage, jpChemgrade);
+        System.out.printf("name: %s \n age: %d \n Chemistry Grade: %f \n \n",
+                person2, Tage, tChemgrade);
+        System.out.printf("name: %s \n age: %d \n Chemistry Grade: %f \n \n",
+                person3, JiAge, jianChemgrade);
+        
+        }
+}
+    
