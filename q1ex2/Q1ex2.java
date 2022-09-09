9e957fbeeecdb64a1d78648c1cb03c24aa914360
Kendronks <80277638+Kendronks@users.noreply.github.com>
2022-09-09 12:15:20 +0800

    Add files via upload

---
+++ q1ex2/Q1ex2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q1ex2;
+import java.util.Scanner;
+/**
+ *
+ * @author TAU
+ */
+public class Q1ex2 {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //number generator
+        int random = (int) Math.floor(Math.random()*10) + 1;
+        int guess = 0;
+        int guesses = 3;
+        //cIn
+                
+        //comparison
+        for (int i = 0; i <= guesses; i++) {
+            if(guess == random)
+        }
+        //cOut
+    }
+    
+}
